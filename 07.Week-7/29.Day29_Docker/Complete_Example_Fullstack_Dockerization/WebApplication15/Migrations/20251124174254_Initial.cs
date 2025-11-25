73372d6995841ecad2473009111de843858c3fd6
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-11-25 17:31:50 +0530

    Add files via upload

---
+++ 07.Week-7/29.Day29_Docker/Complete_Example_Fullstack_Dockerization/WebApplication15/Migrations/20251124174254_Initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication15.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PRODUCTS",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    NAME = table.Column<string>(type: "NVARCHAR2(200)", maxLength: 200, nullable: false),
+                    PRICE = table.Column<decimal>(type: "NUMBER(18,2)", nullable: false),
+                    CATEGORY = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PRODUCTS", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PRODUCTS");
+        }
+    }
+}
